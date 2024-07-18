d5564665444f95fb3e968b2e1a07124b4150ff11
Dogus Tabiyat <dogus14.tabiyat@gmail.com>
2024-07-19 00:05:07 +0300

    Final commit

---
+++ js/api.js
@@ -0,0 +1,60 @@
+
+start_button = document.getElementById("start-btn");
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const loading = document.querySelector('#loading');
+
+  fetch('https://codecyprus.org/th/api/list')
+  .then(response => response.json())
+  .then(data => {
+        const tableBody = document.querySelector('#hunts-table tbody');
+        
+        console.log(data);
+        tableBody.innerHTML = '';
+        const header = document.createElement('tr');
+        const nameHeader = document.createElement('th');
+        nameHeader.textContent = 'Name';
+        header.appendChild(nameHeader);
+        const descriptionHeader = document.createElement('th');
+        descriptionHeader.textContent = 'Description';
+        header.appendChild(descriptionHeader);
+        const startsOnHeader = document.createElement('th');
+        startsOnHeader.textContent = 'Starts On';
+        header.appendChild(startsOnHeader);
+        const endsOnHeader = document.createElement('th');
+        endsOnHeader.textContent = 'Ends On';
+        header.appendChild(endsOnHeader);
+        tableBody.appendChild(header);
+
+        data.treasureHunts.forEach(hunt => {
+          const row = document.createElement('tr');
+          const nameCell = document.createElement('td');
+          nameCell.textContent = hunt.name;
+          row.appendChild(nameCell);
+          const descriptionCell = document.createElement('td');
+          descriptionCell.textContent = hunt.description;
+          row.appendChild(descriptionCell);
+          const startsOnCell = document.createElement('td');
+          startsOnCell.textContent = new Date(hunt.startsOn).toLocaleString();
+          row.appendChild(startsOnCell);
+          const endsOnCell = document.createElement('td');
+          endsOnCell.textContent = new Date(hunt.endsOn).toLocaleString();
+          row.appendChild(endsOnCell);
+          tableBody.appendChild(row);
+          const huntId = hunt.uuid;
+          console.log(huntId);
+
+          row.addEventListener('click', () => {
+            window.location.href = `pre_start.html?treasure-hunt-id=${huntId}`;
+          });
+        });
+    })
+    .catch(error => console.error('Error fetching data:', error))
+    .finally(() => {
+      loading.style.display = 'none';
+  });
+});
+
+
+
